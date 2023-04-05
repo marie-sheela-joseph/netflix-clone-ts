42404013c66ff2f036281844bbdc75719aedd3b2
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-04-05 14:08:22 +0200

    banner completed

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -1,51 +1,44 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react'
+import './banner.css'
+import { requests } from './requests'
+type banner={
+    title?:string,
+    name?:string,
+    original_name?:string,
+    overview:string,
+    backdrop_path:string
+}
 function Banner() {
-  return (
-    <div>Banner</div>
-  )
+    const [state, setState] = useState<banner>({overview:'',backdrop_path:''})
+    useEffect(() => {
+        fetch(requests.fetchNetflixOriginals)
+            .then(res => res.json())
+            .then(data => {
+                setState(data.results[Math.floor(Math.random() * 10) + 1])
+            })
+    }, [])
+    function truncate(str:string, length:number) {
+        return str?.length > length ? str.substr(0, length - 1) + '...' : str
+    }
+    return (
+        <header className='banner' style={{
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${state?.backdrop_path}')`,
+            backgroundSize: 'cover',
+            backgroundPosition: `center center`
+        }}>
+            <div className='banner__content'>
+                <h1 className='banner__title'>{state?.title || state?.name || state?.original_name}</h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My List</button>
+                </div>
+                <h1 className='banner__description'>
+                    {truncate(state?.overview, 150)}
+                </h1>
+            </div>
+            <div className='banner__fadeBotttom'></div>
+        </header >
+    )
 }
 
 export default Banner
-// import React, { useEffect, useState } from 'react'
-// import './banner.css'
-// import { requests } from './requests'
-// type banner={
-//     title?:string,
-//     name?:string,
-//     original_name?:string
-// }
-// function Banner() {
-//     const [state, setState] = useState([])
-//     useEffect(() => {
-//         fetch(requests.fetchNetflixOriginals)
-//             .then(res => res.json())
-//             .then(data => {
-//                 setState(data.results[Math.floor(Math.random() * 10) + 1])
-//             })
-//     }, [])
-//     function truncate(str, length) {
-//         return str?.length > length ? str.substr(0, length - 1) + '...' : str
-//     }
-//     return (
-//         <header className='banner' style={{
-//             backgroundImage: `url('https://image.tmdb.org/t/p/original/${state?.backdrop_path}')`,
-//             backgroundSize: 'cover',
-//             backgroundPosition: `center center`
-//         }}>
-//             <div className='banner__content'>
-//                 <h1 className='banner__title'>{state?.title || state?.name || state?.original_name}</h1>
-//                 <div className='banner__buttons'>
-//                     <button className='banner__button'>Play</button>
-//                     <button className='banner__button'>My List</button>
-//                 </div>
-//                 <h1 className='banner__description'>
-//                     {truncate(state?.overview, 150)}
-//                 </h1>
-//             </div>
-//             <div className='banner__fadeBotttom'></div>
-//         </header >
-//     )
-// }
-
-// export default Banner
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './nav.css'
 import { useNavigate } from 'react-router-dom'
 function Nav() {
